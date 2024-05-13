f8ab4fcb2bb144c637aa8486ab7bc56f8ea091ff
Ibrahima17 <95368838+Ibrahima17@users.noreply.github.com>
2024-05-13 10:47:10 +0200

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,18 +1,25 @@
 #include "./../../include/calculator.h"
 
-double _add(double a, double b) {
-	return a+b;
+int addition(int a, int b) {
+    return a + b;
 }
 
-double _sub(double a, double b) {
-	return a-b;
+int soustraction(int a, int b) {
+    return a - b;
 }
 
-double _mul(double a, double b) {
-	return a*b;
+int multiplication(int a, int b) {
+    return a * b;
 }
 
-double _div(double a, double b) {
-	return a/b;
+int division(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0; // Gérer la division par zéro
+    }
 }
-	
+
+int carre(int a) {
+    return a * a; // Implémentation de la fonction au carré
+}
