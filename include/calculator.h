04ff2ff7a54a463205bc76eb8557f2e281ab2c6b
Ibrahima17 <95368838+Ibrahima17@users.noreply.github.com>
2024-05-13 10:46:28 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,10 @@
-double _add(double a, double b);
-double _sub(double a, double b);
-double _mul(double a, double b);
-double _div(double a, double b);
+#ifndef CALCULATRICE_H
+#define CALCULATRICE_H
+
+int addition(int a, int b);
+int soustraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b);
+int carre(int a); // Prototype de la nouvelle fonction
+
+#endif /* CALCULATRICE_H */
